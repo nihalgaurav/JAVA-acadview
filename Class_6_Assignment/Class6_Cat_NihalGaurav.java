ee0f9a93041e6e689daaea2b9aad832dd6b6def3
satyam raj <33103451+SATYAM6316071@users.noreply.github.com>
2018-10-30 19:24:00 +0530

    Update Class6_Cat_NihalGaurav.java

--- Class_6_Assignment/Class6_Cat_NihalGaurav.java
+++ Class_6_Assignment/Class6_Cat_NihalGaurav.java
@@ -2,7 +2,7 @@ package Class_6Assignment;
 
 public class Class6_Cat_NihalGaurav extends Class6_Animal_NihalGaurav{
 	void speak() {
-		System.out.println("Cat is speaking ");
+		System.out.println("Cat speaks very loud");
 	}
 
 

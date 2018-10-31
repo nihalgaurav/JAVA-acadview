657cbf122ca8b0c0ed317ec0f325ba8b836a46b7
Poonam Kaur <36960022+poonam5248@users.noreply.github.com>
2018-10-31 20:30:12 +0530

    Create count.java

---
+++ count.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Count {
+		
+	    public static int countWords(String str)
+	    {
+	        int count = 1;
+	        for (int i=0;i<=str.length()-1;i++)
+	        {
+	            if (str.charAt(i) == ' ' && str.charAt(i+1)!=' ')
+	            {
+	                count++;
+	            }
+	        }
+	        return count;
+	    }
+	    public static void main(String[] args)
+	    {
+	        Scanner in = new Scanner(System.in);
+	        System.out.print("Enter a sentence: ");
+	        String sentence = in.next();
+	        System.out.print("Your sentence has " + countWords(sentence) + " words.");
+	    }
+	}
